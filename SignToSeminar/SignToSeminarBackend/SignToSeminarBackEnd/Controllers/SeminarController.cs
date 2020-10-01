0f84978148d50c6ea51a304151b626707a0f6dbd
moapng <larssonmoaml@gmail.com>
2020-10-01 11:46:24 +0200

    Added some methods
    search for specific seminar, add seminar, delete seminar

--- SignToSeminar/SignToSeminarBackend/SignToSeminarBackEnd/Controllers/SeminarController.cs
+++ SignToSeminar/SignToSeminarBackend/SignToSeminarBackEnd/Controllers/SeminarController.cs
@@ -20,11 +20,41 @@ namespace SignToSeminarBackEnd.Controllers
             _context = context;
         }
 
+        //få fram alla seminars namn
         [HttpGet]
         public IEnumerable<SeminarModel> Get()
         {
             return _context.Seminars.Include(s => s.name).ToArray<SeminarModel>();
         }
+
+        //få fram specifikt seminar mha id
+        [HttpGet("{id}")]
+        public SeminarModel Get(int id)
+        {
+            var seminar = _context.Seminars.Where(s => s.id == id).FirstOrDefault();
+            if (seminar != null)
+                return seminar;
+            else
+                return null;
+        }
+
+        //lägg till seminar
+        [HttpPost]
+        public void Post([FromBody] SeminarModel seminar)
+        {
+            _context.Seminars.Add(seminar);
+            _context.SaveChanges();
+        }
+
+        //ta bort specifikt seminar mha id
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var seminar = new SeminarModel { id = id };
+            _context.Seminars.Attach(seminar);
+            _context.Seminars.Remove(seminar);
+            _context.SaveChanges();
+        }
     }
-    
+
 }
